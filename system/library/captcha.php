9e3336d679f1452c4b6e37782cb2850a2ca71073
muniraj <muniraj14@gmail.com>
2017-01-07 15:37:30 +0530

    captcha update

--- system/library/captcha.php
+++ system/library/captcha.php
@@ -10,9 +10,13 @@ class Captcha {
 	}
 
 	function getCode(){
-		return $this->code;
-	}
+		$out = ob_get_contents();
+		$out = str_replace(array("\n", "\r", "\t", " "), "", $this->code);
+		ob_end_clean();
 
+		   return $out;
+	}
+	
 	function showImage() {
         $image = imagecreatetruecolor($this->height, $this->width);
 
